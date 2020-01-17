5c8b0f227208ed0426edbdd8035e42107362810e
Benjamin Huang <bhuang@rms.com>
2020-01-17 09:41:58 -0800

    add method

--- todo-app/app.py
+++ todo-app/app.py
@@ -3,14 +3,61 @@ from chalice import Chalice
 app = Chalice(app_name='todo-app')
 
 
+TODO_ITEMS = {
+    '1': {
+        'item': 'buy milk'
+    },
+    '2': {
+        'item': 'mow lawn'
+    }
+}
+
 @app.route('/')
 def index():
-    return {'hello': 'world'}
+    print("Calling our index route")
+    return {'hello': 'serverless world'}
 
-
-@app.route('/')
+@app.route('/todo')
 def todos():
-  return 
+    items = [ v for k, v in TODO_ITEMS.items() ]
+
+    params = app.current_request.query_params
+    if params:
+        offset = int(params.get('offset', 0))
+        size = int(params.get('size', len(TODO_ITEMS)))
+
+        return items[offset:size]
+
+    return items
+
+@app.route('/todo/{todo_id}')
+def get_todo(todo_id):
+    return TODO_ITEMS[todo_id]
+
+@app.route('/todo/{todo_id}', methods=["DELETE"])
+def delete_todo(todo_id):
+    item = TODO_ITEMS[todo_id]
+    del TODO_ITEMS[todo_id]
+    return item
+
+@app.route('/todo/{todo_id}', methods=["POST", "PUT"])
+def update_todo(todo_id):
+    if app.current_request.method == "POST":
+        TODO_ITEMS[todo_id].update(app.current_request.json_body)
+    else:
+        TODO_ITEMS[todo_id] = app.current_request.json_body
+    return TODO_ITEMS[todo_id]
+
+@app.route('/introspect')
+def introspect():
+    return app.current_request.to_dict()
+
+@app.route('/todo', methods=["POST"])
+def add_todo():
+    todo = app.current_request.json_body
+    new_id = str(len(TODO_ITEMS) + 1)
+    TODO_ITEMS[new_id] = todo
+    return todo
 
 # The view function above will return {"hello": "world"}
 # whenever you make an HTTP GET request to '/'.
